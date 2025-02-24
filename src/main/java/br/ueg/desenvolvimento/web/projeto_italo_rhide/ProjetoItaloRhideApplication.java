2cd1f30daa1492fb396bd12041409f23c1821b9a
Italo Rhide <rhideitalo@gmail.com>
2025-02-24 20:24:41 -0300

    start

---
+++ src/main/java/br/ueg/desenvolvimento/web/projeto_italo_rhide/ProjetoItaloRhideApplication.java
@@ -0,0 +1,13 @@
+package br.ueg.desenvolvimento.web.projeto_italo_rhide;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoItaloRhideApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoItaloRhideApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/ueg/desenvolvimento/web/projeto_italo_rhide/ProjetoItaloRhideApplicationTests.java
@@ -0,0 +1,13 @@
+package br.ueg.desenvolvimento.web.projeto_italo_rhide;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoItaloRhideApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
