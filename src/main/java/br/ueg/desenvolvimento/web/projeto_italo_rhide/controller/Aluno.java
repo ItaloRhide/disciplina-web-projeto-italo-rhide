0df82fbb260bb15963ade12f2c33e8a6f6b78b85
Italo Rhide <rhideitalo@gmail.com>
2025-05-11 16:02:04 -0300

    end

--- src/main/java/br/ueg/desenvolvimento/web/projeto_italo_rhide/controller/Aluno.java
+++ src/main/java/br/ueg/desenvolvimento/web/projeto_italo_rhide/controller/Aluno.java
@@ -1,9 +1,13 @@
 package br.ueg.desenvolvimento.web.projeto_italo_rhide.controller;
 
+import java.util.List;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.OneToMany;
 
 @Entity
 public class Aluno {
@@ -15,6 +19,12 @@ public class Aluno {
     private String nome;
     private String email;
 
+    @OneToMany(mappedBy = "aluno")
+    private List<TelefoneAluno> telefones;
+
+    @ManyToMany
+    private List<Disciplina> disciplinas;
+
     public Aluno() {
 
     }
@@ -22,6 +32,22 @@ public class Aluno {
     public Aluno(String nome, String email) {
         this.nome = nome;
         this.email = email;
+    }
+
+    public List<TelefoneAluno> getTelefones() {
+        return telefones;
+    }
+
+    public void setTelefones(List<TelefoneAluno> telefones) {
+        this.telefones = telefones;
+    }
+
+    public List<Disciplina> getDisciplinas() {
+        return disciplinas;
+    }
+
+    public void setDisciplinas(List<Disciplina> disciplinas) {
+        this.disciplinas = disciplinas;
     }
 
     public Integer getId() {
--- src/main/java/br/ueg/desenvolvimento/web/projeto_italo_rhide/controller/AlunoController.java
+++ src/main/java/br/ueg/desenvolvimento/web/projeto_italo_rhide/controller/AlunoController.java
@@ -51,22 +51,63 @@ public class AlunoController {
         return "redirect:/alunos";
     }
 
+    @Autowired
+    private DisciplinaRepository disciplinaRepository;
+
     @GetMapping("/alunos/update/{id}")
     public String getUpdate(@PathVariable int id, Model model) {
         // model.addAttribute("aluno", alunos.get(id));
         model.addAttribute("aluno", alunoRepository.findById(id).get());
         model.addAttribute("id", id);
+        model.addAttribute("todasDisciplinas", disciplinaRepository.findAll());
         return "aluno-update.html";
     }
 
     @PostMapping("/alunos/update")
-    public String postUpdate(@RequestParam int id, @RequestParam String nome, @RequestParam String email) {
+    public String postUpdate(@RequestParam int id,
+            @RequestParam String nome,
+            @RequestParam String email,
+            @RequestParam List<Integer> disciplinas) {
         // alunos.set(id, Map.of("nome", nome, "email", email));
         Aluno aluno = alunoRepository.findById(id).get();
         aluno.setNome(nome);
         aluno.setEmail(email);
+        List<Disciplina> disciplinasBd = new ArrayList<>();
+        for (Integer discId : disciplinas) {
+            Disciplina disciplinaBd = disciplinaRepository.findById(discId).get();
+            disciplinasBd.add(disciplinaBd);
+        }
+        aluno.setDisciplinas(disciplinasBd);
         alunoRepository.save(aluno);
         return "redirect:/alunos";
+    }
+
+    @GetMapping("/alunos/update/telefone/{id}")
+    public String updateTelefone(@PathVariable Integer id, Model model) {
+        Aluno aluno = alunoRepository.findById(id).orElse(null);
+        model.addAttribute("aluno", aluno);
+        return "aluno-telefone";
+    }
+
+    @Autowired
+    TelefoneAlunoRepository telefoneAlunoRepository;
+
+    @PostMapping("/alunos/update/telefone")
+    public String postUpdateTelefone(@RequestParam int id,
+            @RequestParam String novoTelefone) {
+        Aluno aluno = alunoRepository.findById(id).get();
+        TelefoneAluno telefone = new TelefoneAluno();
+        telefone.setNumero(novoTelefone);
+        telefone.setAluno(aluno);
+        telefoneAlunoRepository.save(telefone);
+        return "redirect:/alunos/update/telefone/" + id;
+    }
+
+    @GetMapping("/alunos/delete/telefone/{id}/{idTelefone}")
+    public String deleteTelefone(@PathVariable int id,
+            @PathVariable int idTelefone) {
+        telefoneAlunoRepository.deleteById(idTelefone);
+        return "redirect:/alunos/update/telefone/" + id;
     }
 
     @GetMapping("/alunos/delete/{id}")
